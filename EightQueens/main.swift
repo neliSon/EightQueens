45100e75686b3061e68e61c92b11f01a6c7ae1c1
neliSun <nelson.chow91.nc@gmail.com>
2017-03-24 17:36:02 -0700

    Initial Commit

---
+++ EightQueens/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EightQueens
+//
+//  Created by Nelson Chow on 2017-03-24.
+//  Copyright © 2017 Nelson Chow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
